5e843a8dc3ccc094d0e8ff668f3b7e56b803727c
Shivashankar P <142704081+shivashankarP2006@users.noreply.github.com>
2026-03-07 20:47:08 +0530

    Fix indentation and update user keys in auth.py

--- Startup client managment/auth.py
+++ Startup client managment/auth.py
@@ -10,16 +10,16 @@ USERS = {
     "tl4": {"password": "tl123", "role": "Team Lead"},
     "tl5": {"password": "tl123", "role": "Team Lead"},
 
-    "member1": {"password": "mem123", "role": "Team Member"},
-    "member2": {"password": "mem123", "role": "Team Member"},
-    "member3": {"password": "mem123", "role": "Team Member"},
-    "member4": {"password": "mem123", "role": "Team Member"},
-    "member5": {"password": "mem123", "role": "Team Member"},
-    "member6": {"password": "mem123", "role": "Team Member"},
-    "member7": {"password": "mem123", "role": "Team Member"},
-    "member8": {"password": "mem123", "role": "Team Member"},
-    "member9": {"password": "mem123", "role": "Team Member"},
-    "member10": {"password": "mem123", "role": "Team Member"}
+    "mem1": {"password": "mem123", "role": "Team Member"},
+    "mem2": {"password": "mem123", "role": "Team Member"},
+    "mem3": {"password": "mem123", "role": "Team Member"},
+    "mem4": {"password": "mem123", "role": "Team Member"},
+    "mem5": {"password": "mem123", "role": "Team Member"},
+    "memb": {"password": "mem123", "role": "Team Member"},
+    "mem7": {"password": "mem123", "role": "Team Member"},
+    "mem8": {"password": "mem123", "role": "Team Member"},
+    "mem9": {"password": "mem123", "role": "Team Member"},
+    "mem10": {"password": "mem123", "role": "Team Member"}
 }
 
 def login():
@@ -44,4 +44,5 @@ def login():
             st.rerun()
 
         else:
+
             st.error("Invalid credentials")
